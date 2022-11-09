02a3c56c8ba63c54b5f8fbb0e66f7c104fab80c7
Aguimarcenes <aguinaldo.marcenes@gmail.com>
2022-11-08 22:58:16 -0300

    Fixing a Bug

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -4,31 +4,31 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonToLi(pokemon){
-    return `<li class="pokemon"> 
-    <span class="number">#001</span>
-    <span class="name">${pokemon.name}</span>    
-    <div class="detail"> 
-            <ol class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
-            </ol>       
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
-            alt="${pokemon.name}">        
-    </div>
-    </li>`
+function convertPokemonToHtml(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>    
 
+            <div class="detail"> 
+                    <ol class="types">
+                            <li class="type">Grass</li>
+                            <li class="type">Poison</li>
+                    </ol> 
+                        
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">        
+            </div>
+        </li>
+    `
 }
-
-const pokemonList = document.getElementById('pokemonList')
-
+const list = document.querySelector('.pokemons')
 fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.lenght; i++){
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+    .then((pokemonsList) => {
+        for (const pokemon of pokemonsList) {
+            const html = convertPokemonToHtml(pokemon)
+            list.innerHTML += html
         }
     })
     .catch((error) => console.log(error))
